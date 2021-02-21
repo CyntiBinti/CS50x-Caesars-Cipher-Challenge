15a5e3010bf6d77371b7c9d36a0319b424064a7b
Cynthia Mbulu <72040367+CyntiBinti@users.noreply.github.com>
2021-02-21 22:45:55 +0000

    Update caesar.c

--- caesar.c
+++ caesar.c
@@ -8,13 +8,21 @@ int main(int argc, string argv[]) {
     
     //run command = argv[0], and the user input (of the key) is argv[1]
     if (argc == 2) {
+        
+        int i;
+        int digit_number = 0;
+        
         for (i = 0; i < strlen(argv[1]); i++)
         {
-            //iterate over each character in the string held in argv[1] to make sure it's a digit
+            //iterate over each character in the string held in argv[1] to make sure it's a digit and if so then print string as int
             if (isdigit(argv[1][i]))
             {
+                digit_number +=1;
+            }
+            if (digit_number == strlen(argv[1]))
+            {
                 printf("Success\n");
-                printf("%s\n", argv[1]);
+                printf("%i\n", atoi(argv[1]));
                 return 0;   
             }
             
