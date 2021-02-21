c943bb5d059b2f870dc235723519e101e634b171
Cynthia Mbulu <72040367+CyntiBinti@users.noreply.github.com>
2021-02-21 23:36:21 +0000

    Update caesar.c

--- caesar.c
+++ caesar.c
@@ -7,8 +7,7 @@
 int main(int argc, string argv[]) {
     
     int i;
-    int digit_number = 0;
-        
+
     //the run command is stored in argv[0], and the user input (of the cipher key) is stored in argv[1]
     if (argc == 2) {
         
@@ -16,25 +15,14 @@ int main(int argc, string argv[]) {
         for (i = 0; i < strlen(argv[1]); i++)
         {
             //make sure the [i]th character is a digit and if so then print string as int instead
-            if (isdigit(argv[1][i]))
+            if (!isdigit(argv[1][i]))
             {
-                //counts the number of digits in argv[1]
-                digit_number += 1;
+                printf("Usage: ./caesar key");
+                return 1;
             }
         }
-        //if the number of digits counted = the number of characters in argv[1] then the Boolean = true
-        if (digit_number == strlen(argv[1]))
-        {
             printf("Success\n");
             printf("%i\n", atoi(argv[1]));
             return 0;   
-        }
     }
-        
-    //if [i]th character contains a non-digit then the Boolean = false so the for loop terminates and below error message appears
-    else {
-        printf("Usage: ./caesar key");
-        return 1;
-    }
-    
- }
+}
